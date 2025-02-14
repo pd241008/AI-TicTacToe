ff537d426f4a3cbd9281a3e51dcd73af2ab2b12b
Prathmesh Desai <142252602+pd241008@users.noreply.github.com>
2025-02-14 23:33:29 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import QuizApp from "./components/QuizPage";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <QuizApp></QuizApp>
+    </div>
+  );
+}
+
+export default App;
