5f7f3ce772b2c75601799c50bd435527a1df771e
pd241008 <142252602+pd241008@users.noreply.github.com>
2025-08-16 21:33:29 +0530

    Revert "Delete app directory"
    This reverts commit 610206998f9af5628d7e748db0a41d0052230e39.

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPage from "@/components/Landingpage";
+
+const page = () => {
+  return (
+    <div>
+      <main>
+        <LandingPage />
+      </main>
+    </div>
+  );
+};
+
+export default page;
